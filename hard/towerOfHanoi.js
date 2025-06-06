4a0f6b4025ce3b69b80a1bab8827394c0b5544c8
muuo <anthonymuuo92@gmail.com>
2025-06-06 12:49:04 +0300

    tower of hanoi

---
+++ hard/towerOfHanoi.js
@@ -0,0 +1,7 @@
+function towerHanoi(disks) {
+  return 2 ** disks - 1;
+}
+console.log(towerHanoi(3));
+console.log(towerHanoi(5));
+
+//could play the game here: https://www.mathsisfun.com/games/towerofhanoi.html
