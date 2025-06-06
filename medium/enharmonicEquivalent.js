4bd98c56723d96876e2ed30f3c30fd607e7485a2
muuo <anthonymuuo92@gmail.com>
2025-06-06 11:54:03 +0300

    equivalent with object,,just learned should use [] when you dont know the key untill runtime

--- medium/enharmonicEquivalent.js
+++ medium/enharmonicEquivalent.js
@@ -1,25 +1,47 @@
+// function getEquaivalent(note) {
+//   if (note === "C#") {
+//     return "Db";
+//   } else if (note === "Db") {
+//     return "C#";
+//   } else if (note === "D#") {
+//     return "Eb";
+//   } else if (note === "Eb") {
+//     return "D#";
+//   } else if (note === "F#") {
+//     return "Gb";
+//   } else if (note === "Gb") {
+//     return "F#";
+//   } else if (note === "G#") {
+//     return "Ab";
+//   } else if (note === "Ab") {
+//     return "G#";
+//   } else if (note === "A#") {
+//     return "Bb";
+//   } else if (note === "Bb") {
+//     return "A#";
+//   }
+// }
+// console.log(getEquaivalent("D#"));
+// console.log(getEquaivalent("Gb"));
+// console.log(getEquaivalent("Bb"));
+
+//much simpler way using objectKeys
+
+const equivalents = {
+  "C#": "Db",
+  Db: "C#",
+  "D#": "Eb",
+  Eb: "D#",
+  "F#": "Gb",
+  Gb: "F#",
+  "G#": "Ab",
+  Ab: "G#",
+  "A#": "Bb",
+  Bb: "A#",
+};
+
 function getEquaivalent(note) {
-  if (note === "C#") {
-    return "Db";
-  } else if (note === "Db") {
-    return "C#";
-  } else if (note === "D#") {
-    return "Eb";
-  } else if (note === "Eb") {
-    return "D#";
-  } else if (note === "F#") {
-    return "Gb";
-  } else if (note === "Gb") {
-    return "F#";
-  } else if (note === "G#") {
-    return "Ab";
-  } else if (note === "Ab") {
-    return "G#";
-  } else if (note === "A#") {
-    return "Bb";
-  } else if (note === "Bb") {
-    return "A#";
-  }
+  return equivalents[note];
 }
 console.log(getEquaivalent("D#"));
 console.log(getEquaivalent("Gb"));
