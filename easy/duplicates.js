039ed66a7e7381e161b3cf81cf5ff513374826dd
muuo <anthonymuuo92@gmail.com>
2025-06-10 21:44:34 +0300

    learned that Set receives only unique values and that this has a bigO of 0n

--- easy/duplicates.js
+++ easy/duplicates.js
@@ -1,13 +1,28 @@
-function hasDuplicates(array) {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[i] === array[j]) {
-        return true;
-      }
+// function hasDuplicates(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[i] === array[j]) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+
+// console.log(hasDuplicates([1, 2, 3, 3])); //true
+// console.log(hasDuplicates([1, 2, 3, 4])); //false
+
+// Hash Set for good time and space complexity
+
+function hasDuplicates(nums) {
+  const seen = new Set();
+  for (const num of nums) {
+    if (seen.has(num)) {
+      return true;
     }
+    seen.add(num);
   }
   return false;
 }
-
 console.log(hasDuplicates([1, 2, 3, 3])); //true
 console.log(hasDuplicates([1, 2, 3, 4])); //false
