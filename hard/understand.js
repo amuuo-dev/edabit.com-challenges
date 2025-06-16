2d6948b37d1dbcf9c1b700284c8e1200ea7898bc
muuo <anthonymuuo92@gmail.com>
2025-06-16 21:06:49 +0300

    so all fields are converted to a number- loose equality is weird

--- hard/understand.js
+++ hard/understand.js
@@ -1,5 +1,6 @@
 // console.log(018 - 015);
 // console.log(typeof typeof 1);
-const numbers = [33, 2, 8];
-numbers.sort();
-console.log(numbers[1]);
+// const numbers = [33, 2, 8];
+// numbers.sort();
+// console.log(numbers[1]);
+console.log(false == "0");
